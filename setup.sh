993488aa2e22624c022dadb6b148a650a79a1612
taiki1280 <taiki.kawagishi.private@gmail.com>
2023-02-26 22:42:58 +0900

    [add] .gitconfig & [update] setup.sh

--- setup.sh
+++ setup.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/zsh
-DOT_FILES=(.zshrc .vimrc)
+DOT_FILES=(.zshrc .vimrc .gitconfig)
 BACKUP_DIR=~/dotfiles_bak/
 
 for file in ${DOT_FILES[@]}
@@ -16,8 +16,9 @@ do
     fi
     ln -svf ~/git/taiki/dotfiles/$file ~/
     echo 'created symbolic link'
-    if [ $file != ".vimrc" ]; then
+    if [ $file = '.zshrc' ]; then
         # 「zsh」は「.vimrc」に非対応。また、「.vimrc」は起動する度に読み込むらしいので不要
+        # 「.gitconfig」もsourceする必要は無いと思ったため読み込む.zshrcのみへ修正
         echo "run next command 'source ~/${file}'"
         source ~/$file
     fi
