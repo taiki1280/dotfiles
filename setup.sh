d8128fb28a33a6e3f6e3b82c32949660935c5b45
taiki1280 <taiki.kawagishi.private@gmail.com>
2023-07-23 02:26:33 +0900

    [add] .prettierrc

--- setup.sh
+++ setup.sh
@@ -1,24 +1,24 @@
 #!/usr/bin/zsh
-DOT_FILES=(.zsh_profile .zshrc .vimrc .gitconfig)
-BACKUP_DIR=~/dotfiles_bak/
+DOT_FILES=(.zsh_profile .zshrc .vimrc .gitconfig .prettierrc)
+BACKUP_DIR=$HOME/dotfiles_bak/
 
 for file in ${DOT_FILES[@]}; do
-  if [ -f ~/$file ]; then
-    echo -e "already exists「~/${file}」\nSo,will create backup files and overwrite save these."
+  if [ -f $HOME/$file ]; then
+    echo -e "already exists「$HOME/$file」\nSo,will create backup files and overwrite save these."
     if [ ! -d $BACKUP_DIR ]; then
       # 存在しない場合は作成
       mkdir -v $BACKUP_DIR
       echo 'created directory for backup'
     fi
-    cp ~/$file $BACKUP_DIR$file
-    echo "created right one 「${BACKUP_DIR}${file}」"
+    cp $HOME/$file $BACKUP_DIR$file
+    echo "created right one 「${BACKUP_DIR}$file」"
   fi
-  ln -svf ~/git/taiki/dotfiles/$file ~/
+  ln -svf $HOME/git/taiki/dotfiles/$file $HOME/
   echo 'created symbolic link'
   if [ $file = '.zshrc' ]; then
     # 「zsh」は「.vimrc」に非対応。また、「.vimrc」は起動する度に読み込むらしいので不要
     # 「.gitconfig」もsourceする必要は無いと思ったため読み込む.zshrcのみへ修正
-    echo "run next command 'source ~/${file}'"
-    source ~/$file
+    echo "run next command 'source $HOME/$file'"
+    source $HOME/$file
   fi
 done
