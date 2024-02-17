6ce9ca703e8f8440e5a564cd4bdcdbdd3c3c6b1f
taiki1280 <taiki.kawagishi.private@gmail.com>
2024-02-18 03:46:18 +0900

    [update] 以下の更新
    1. 指定したファイルのみではなく、リポジトリ直下のすべてのドットファイルを読み込む様に修正
    2. 実行結果の出力の体裁を修正

--- install.sh
+++ install.sh
@@ -1,24 +1,36 @@
 #!/usr/bin/zsh
-DOT_FILES=(.zsh_profile .zshrc .vimrc .gitconfig .prettierrc)
-BACKUP_DIR=$HOME/dotfiles_bak/
+DOT_FILES=($(ls -d $(ls -aF | grep -v "/" | grep -e "^\..\+$")))
+echo '同階層にあるすべての dotfiles を処理します。'
+echo '\t''1. ホームディレクトリ直下に既に同じ名前のファイルがある場合、バックアップを作成して移動'
+echo '\t''2. ドットファイルの新保陸リンクを作成'
+echo '\t''3. .zshrc がある場合、読み込み'
+echo '-----------------------------------------------------------------------------------'
+BACKUP_DIR=$HOME/dotfiles_bk.d/
+message_result=''
 
 for file in ${DOT_FILES[@]}; do
+  message_result="${message_result}${file}"'\n'
   if [ -f $HOME/$file ]; then
-    echo -e "already exists「$HOME/$file」\nSo,will create backup files and overwrite save these."
     if [ ! -d $BACKUP_DIR ]; then
-      # 存在しない場合は作成
+      # バックアップのディレクトリが存在しない場合は作成
       mkdir -v $BACKUP_DIR
-      echo 'created directory for backup'
+      message_result="${message_result}"'\t''created directory for backup''\n'
     fi
-    cp $HOME/$file $BACKUP_DIR$file
-    echo "created right one 「${BACKUP_DIR}$file」"
+    message_result="${message_result}"'\t'"already exists「$HOME/$file」"'\n'
+    message_result="${message_result}"'\t''So, move it to backup directory & create a new one.''\n'
+    # 予め作成しているバックアップディレクトリへ移動させる
+    mv $HOME/$file $BACKUP_DIR$file
   fi
-  ln -svf $PWD/$file $HOME/
-  echo 'created symbolic link'
-  if [ $file = '.zshrc' ]; then
-    # 「zsh」は「.vimrc」に非対応。また、「.vimrc」は起動する度に読み込むらしいので不要
-    # 「.gitconfig」もsourceする必要は無いと思ったため読み込む.zshrcのみへ修正
-    echo "run next command 'source $HOME/$file'"
-    source $HOME/$file
-  fi
+  result=$(ln -svf $PWD/$file $HOME/)
+  message_result="${message_result}"'\t'"created: ${result}"'\n'
 done
+
+echo '【実行結果】'
+echo $message_result
+echo '-----------------------------------------------------------------------------------'
+
+if [ -e $HOME/.zshrc ]; then
+  echo '最後に .zshrc を読み込みます'
+  echo '-----------------------------------------------------------------------------------'
+  source $HOME/.zshrc
+fi
