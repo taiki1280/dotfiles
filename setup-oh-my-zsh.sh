ae82dcad958f0d6f14a0a886994cc04ce91ad173
taiki1280 <taiki.kawagishi.private@gmail.com>
2024-02-28 12:56:49 +0000

    [update] jonathan theme が気に入ったので固定

--- setup-oh-my-zsh.sh
+++
@@ -1,5 +0,0 @@
-sudo apt -y install zsh
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
-# plugin
-git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
-git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
